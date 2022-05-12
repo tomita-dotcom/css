b94f224cefd9086ddd72bbbc3ae5df0a128833d3
tomita-dotcom <springroll1816@gmail.com>
2022-05-12 23:13:11 +0900

    5.12.5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-const openBth = document.getElementById('openBth');
-const closeBth = document.getElementById('closeBth');
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
 const modal = document.getElementById('modal');
-openBth.addEventListener('click', () => {
+openBtn.addEventListener('click', () => {
   modal.style.display = 'block';
 })
 
-closeBth.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 })
 
 window.addEventListener('click', (e) => {
-  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBth") {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
     modal.style.display = 'none';
   }
 });
